9034ef541d030991b8cbe69643d355ee6610d48d
4794-sourse <2456170006@qq.com>
2021-01-03 18:20:45 +0800

    致命bug

---
+++ bug.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: bug.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 03 Jan 2021 06:20:09 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+
+    return 0;
+}
